b134f6c7274b901c2de3d9e589a00a0433c52797
Quyen Ly <quocquyenit@gmail.com>
2021-07-27 18:07:43 +0700

    Cloned all assertions from https://github.com/IHTSDO/release-validation-framework.git

---
+++ scripts/component-centric/component-centric-snapshot-mrcm-attribute-range-refset-validate-concept-ids.sql
@@ -0,0 +1,2 @@
+call validateConceptIdsInMRCMAttributeRangeRefsetExpression_procedure(<RUNID>,'<ASSERTIONUUID>','mrcmattributerangerefset_s','rangeConstraint','MRCM Attribute Range Refset');
+call validateConceptIdsInMRCMAttributeRangeRefsetExpression_procedure(<RUNID>,'<ASSERTIONUUID>','mrcmattributerangerefset_s','attributeRule','MRCM Attribute Range Refset');
