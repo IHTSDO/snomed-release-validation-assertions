5ea7fba52c96142e43690303d1761dc3abd0e317
Quyen Ly <quocquyenit@gmail.com>
2022-01-07 14:21:29 +0700

    VAL-222 Updated assertion

--- scripts/component-centric/component-centric-snapshot-historical-association-unique-indicator.sql
+++ scripts/component-centric/component-centric-snapshot-historical-association-unique-indicator.sql
@@ -15,7 +15,7 @@ select distinct
 	a.id,
     'curr_associationrefset_s'
 from curr_associationrefset_s a inner join (
-											select min(id) id, referencedComponentId, targetcomponentId, refsetid
+											select id, referencedComponentId, targetcomponentId, refsetid, active, effectivetime
 											from curr_associationrefset_s 
 											group by referencedComponentId, targetcomponentId, refsetid
 											having count(*) >= 2
@@ -24,5 +24,7 @@ from curr_associationrefset_s a inner join (
 											and a.targetcomponentId = b.targetcomponentId 
 											and a.refsetid = b.refsetid 
 											and a.id != b.id
-											and a.active = '1'
-											and b.active = '1';
+											and ((a.effectivetime = b.effectivetime and cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s)) /* Detect all new records in current cycle */
+												or (a.active = '1' and b.active = '1' and 
+													((cast(a.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) and cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s)) /* Detect all old records in the past */
+													or (a.effectivetime <> b.effectivetime and (cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) or cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s)))))) /* Detect old and new records */;
