e0f0d1a4bef3cf1fc48203bdca7b5d53c2c1cebe
Quyen Ly <quocquyenit@gmail.com>
2022-01-10 10:28:11 +0700

    VAL-222 Updated assertion: Duplicate historical association indicators

--- scripts/component-centric/component-centric-snapshot-historical-association-unique-indicator.sql
+++ scripts/component-centric/component-centric-snapshot-historical-association-unique-indicator.sql
@@ -11,11 +11,11 @@ select distinct
 	<RUNID>,
 	'<ASSERTIONUUID>',
 	a.referencedComponentId	,
-	concat('MEMBER: id=',a.id, ' in historical association refset member has a duplicated indicator with MEMBER id= ', b.id),
+	concat('MEMBER: id=',a.id, ' in historical association refset member has a duplicated association with MEMBER id= ', b.id),
 	a.id,
     'curr_associationrefset_s'
 from curr_associationrefset_s a inner join (
-											select min(id) id, referencedComponentId, targetcomponentId, refsetid
+											select id, referencedComponentId, targetcomponentId, refsetid, active, effectivetime
 											from curr_associationrefset_s 
 											group by referencedComponentId, targetcomponentId, refsetid
 											having count(*) >= 2
@@ -23,4 +23,8 @@ from curr_associationrefset_s a inner join (
 										on a.referencedComponentId = b.referencedComponentId 
 											and a.targetcomponentId = b.targetcomponentId 
 											and a.refsetid = b.refsetid 
-											and a.id != b.id;
+											and a.id != b.id
+											and ((a.effectivetime = b.effectivetime and cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ) /* Detect all new records in current cycle */
+												or (a.active = '1' and b.active = '1' and 
+													((cast(a.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s ) and cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ) /* Detect all old records in the past */
+													or (a.effectivetime <> b.effectivetime and (cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) or cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ))))) /* Detect old and new records */;
