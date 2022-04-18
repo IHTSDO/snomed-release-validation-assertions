1693ea29fbbe563bbc3c1033b646146b3bf95263
Quyen Ly <quocquyenit@gmail.com>
2022-04-18 15:51:31 +0700

    VAL-251 Allow historical duplication where only one active record exists

--- scripts/file-centric/file-centric-snapshot-association-valid-key.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-key.sql
@@ -14,8 +14,19 @@
 		concat('ReferencedComponentId:', a.referencedcomponentid, ' targetComponentId:', a.targetcomponentid, ' and refsetId:', a.refsetid, ' are duplicated in the association refset snapshot file.'),
 		a.id,
 		'curr_associationrefset_s'
-	from curr_associationrefset_s a 
-	group by a.refsetid , a.referencedcomponentid , a.targetcomponentid
-	having count(id) > 1;
+	from curr_associationrefset_s a inner join (
+											select id, referencedComponentId, targetcomponentId, refsetid, active, effectivetime
+											from curr_associationrefset_s 
+											group by referencedComponentId, targetcomponentId, refsetid
+											having count(*) >= 2
+											) b
+										on a.referencedComponentId = b.referencedComponentId 
+											and a.targetcomponentId = b.targetcomponentId 
+											and a.refsetid = b.refsetid 
+											and a.id != b.id
+											and ((a.effectivetime = b.effectivetime and cast(a.effectivetime as datetime) = (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ) /* Detect all new records in current cycle */
+												or (a.active = '1' and b.active = '1' and 
+													((cast(a.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s ) and cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ) /* Detect all old records in the past which are acitve */
+													or (a.effectivetime <> b.effectivetime and (cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) or cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_associationrefset_s) ))))) /* Detect old and new records which are acitve */;
 	
 	
