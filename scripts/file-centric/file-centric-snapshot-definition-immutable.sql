1d15997d1c9f7a891251c3ca0f1714319a4685f7
Quyen Ly <quocquyenit@gmail.com>
2022-07-29 16:21:59 +0700

    VAL-267 Stop reporting duplication for historical issues where only one component is active for Definition and Description

--- scripts/file-centric/file-centric-snapshot-definition-immutable.sql
+++ scripts/file-centric/file-centric-snapshot-definition-immutable.sql
@@ -16,8 +16,19 @@
 		concat('Definition: id=',a.id, ' references a term which is duplicated.'),
 		a.id,
 		'curr_textdefinition_s'
-	from curr_textdefinition_s a,
-	(select b.term from curr_textdefinition_s b group by b.typeid, b.conceptid, b.languagecode, binary b.term having count(b.id) > 1) duplicate
-	where a.term = duplicate.term
-	and a.active = 1;
+	from curr_textdefinition_s a inner join (
+											select id, typeid, conceptid, languagecode, term, active, effectivetime
+											from curr_textdefinition_s 
+											group by typeid, conceptid, languagecode, binary term
+											having count(*) >= 2
+											) b
+										on a.typeid = b.typeid 
+											and a.conceptid = b.conceptid 
+											and a.languagecode = b.languagecode 
+											and a.term = b.term
+											and a.id != b.id
+											and (((a.active != b.active or (a.active = '1' and b.active = '1')) and a.effectivetime = b.effectivetime and cast(a.effectivetime as datetime) = (select max(cast(effectivetime as datetime)) from curr_textdefinition_s) ) /* Detect all new active records or active + inactive records in current cycle */
+												or (a.active = '1' and b.active = '1' and 
+													((cast(a.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_textdefinition_s ) and cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_textdefinition_s) ) /* Detect all old records in the past which are acitve */
+													or (a.effectivetime <> b.effectivetime and (cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_textdefinition_s) or cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_textdefinition_s) ))))) /* Detect old and new records which are acitve */;
 	commit;
