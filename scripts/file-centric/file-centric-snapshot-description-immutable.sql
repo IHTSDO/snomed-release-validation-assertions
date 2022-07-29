2ca3d8317394a9fa39cd0c5a71239c718f19b86c
Quyen Ly <quocquyenit@gmail.com>
2022-07-29 16:21:59 +0700

    VAL-267 Stop reporting duplication for historical issues where only one component is active for Definition and Description

--- scripts/file-centric/file-centric-snapshot-description-immutable.sql
+++ scripts/file-centric/file-centric-snapshot-description-immutable.sql
@@ -13,13 +13,24 @@
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		a.conceptid,
-		concat('Concept:',a.conceptid, ' has multiple description ids for the same term:', a.term),
+		concat('Description: id=',a.id, ' references a term which is duplicated.'),
 		a.id,
-		'curr_description_d'
-	from curr_description_d a
-	where a.active = '1'
-	group by a.conceptid, a.moduleid, a.languagecode, binary (a.term)
-	having count(a.id) > 1;
+		'curr_description_s'
+	from curr_description_s a inner join (
+											select id, typeid, conceptid, languagecode, term, active, effectivetime
+											from curr_description_s 
+											group by typeid, conceptid, languagecode, binary term
+											having count(*) >= 2
+											) b
+										on a.typeid = b.typeid 
+											and a.conceptid = b.conceptid 
+											and a.languagecode = b.languagecode 
+											and a.term = b.term
+											and a.id != b.id
+											and (((a.active != b.active or (a.active = '1' and b.active = '1')) and a.effectivetime = b.effectivetime and cast(a.effectivetime as datetime) = (select max(cast(effectivetime as datetime)) from curr_description_s) ) /* Detect all new active records or active + inactive records in current cycle */
+												or (a.active = '1' and b.active = '1' and 
+													((cast(a.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_description_s ) and cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_description_s) ) /* Detect all old records in the past which are acitve */
+													or (a.effectivetime <> b.effectivetime and (cast(a.effectivetime as datetime) =  (select max(cast(effectivetime as datetime)) from curr_description_s) or cast(b.effectivetime as datetime) <  (select max(cast(effectivetime as datetime)) from curr_description_s) ))))) /* Detect old and new records which are acitve */;
 	commit;
 	
 	insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
