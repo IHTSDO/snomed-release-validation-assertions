f3dc96eba70b957d6de7462ce03ab8e7742efd36
Quyen Ly <quocquyenit@gmail.com>
2023-05-05 11:38:57 +0700

    VAL-297 Add new assertion: Inactive members must not have any illegal change for inactivation indicator reason

---
+++ scripts/component-centric/component-centric-snapshot-illegal-change-for-description-or-concept-inactivation-reason.sql
@@ -0,0 +1,23 @@
+
+/********************************************************************************
+	component-centric-snapshot-illegal-change-for-description-or-concept-inactivation-reason
+
+	Assertion:
+	Inactive members must not have any illegal change for inactivation indicator reason.
+
+********************************************************************************/
+
+   insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.referencedcomponentid,
+		concat('Attribute Value Refset id=',a.id, ' is a inactive ', if( a.refsetid = '900000000000490003', 'Description', 'Concept'),' inactivation indicator refset member but there is an illegal change for inactivation indicator reason'),
+		a.id,
+        'curr_attributevaluerefset_s'
+	from curr_attributevaluerefset_s a
+	left join prev_attributevaluerefset_s b on a.id = b.id
+	where a.active = '0'
+	and b.active = '0'
+	and a.refsetid in ('900000000000490003','900000000000489007')
+	and a.valueid <> b.valueid;
