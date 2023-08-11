c7c51fee601cd2eccfd45fadcc10a2b6f26d4c17
dionmcm <dion.mcmurtrie@gmail.com>
2023-08-11 16:15:41 +1000

    Updated to refer to components as allowed by the RF2 spec rather than just concepts

--- scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
@@ -3,7 +3,7 @@
 	file-centric-snapshot-association-valid-targetcomponentid
 
 	Assertion:
-	TargetComponentId refers to valid concepts in the ASSOCIATION REFSET snapshot file.
+	TargetComponentId refers to valid components in the ASSOCIATION REFSET snapshot file.
 
 ********************************************************************************/
 	insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
@@ -16,7 +16,10 @@
 		'curr_associationrefset_s'
 	from curr_associationrefset_s a
 	left join curr_concept_s b
-	on a.targetcomponentid = b.id
-	where b.id is null;
+		on a.targetcomponentid = b.id
+    left join curr_description_s c
+        on a.targetcomponentid = c.id
+    left join curr_relationship_s d
+        on a.targetcomponentid = d.id
+	where b.id is null and c.id is null and d.id is null;
 	commit;
-	
