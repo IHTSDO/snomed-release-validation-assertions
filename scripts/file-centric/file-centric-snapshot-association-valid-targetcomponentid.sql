c644e7b3f9bf4aa1458c26e70e545694b4869ca6
dionmcm <dion.mcmurtrie@gmail.com>
2023-08-11 16:22:52 +1000

    fix tabs in file-centric-snapshot-association-valid-targetcomponentid.sql

--- scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
@@ -17,9 +17,9 @@
 	from curr_associationrefset_s a
 	left join curr_concept_s b
 		on a.targetcomponentid = b.id
-    left join curr_description_s c
-        on a.targetcomponentid = c.id
-    left join curr_relationship_s d
-        on a.targetcomponentid = d.id
+	left join curr_description_s c
+        	on a.targetcomponentid = c.id
+	left join curr_relationship_s d
+		on a.targetcomponentid = d.id
 	where b.id is null and c.id is null and d.id is null;
 	commit;
