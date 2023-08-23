7e364f69a2d8f2554bd542f483175aaa01cd3db0
dionmcm <dion.mcmurtrie@gmail.com>
2023-08-23 15:19:29 +1000

    updated to split concept centric refsets from component centric refsets

--- scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
@@ -11,15 +11,39 @@
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		a.referencedcomponentid,
-		concat('ASSOC RS: Targetcomponentid=',a.targetcomponentid, ':Invalid TargetComponentId.'),
+		concat('ASSOC RS: Targetcomponentid=',a.targetcomponentid, ':Invalid TargetComponentId. RefsetId=', a.refsetid),
+		a.id,
+		'curr_associationrefset_s'
+	from curr_associationrefset_s a
+	left join curr_concept_s b
+		on a.targetcomponentid = b.id
+	where b.id is null
+		and a.refsetid in 
+			(900000000000523009, /* POSSIBLY EQUIVALENT TO */
+			900000000000528000, /* WAS A */
+			900000000000530003, /* ALTERNATIVE */
+			900000000000531004) /* REFERS TO */;
+	commit;
+
+	insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
+	select 
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.referencedcomponentid,
+		concat('ASSOC RS: Targetcomponentid=',a.targetcomponentid, ':Invalid TargetComponentId. RefsetId=', a.refsetid),
 		a.id,
 		'curr_associationrefset_s'
 	from curr_associationrefset_s a
 	left join curr_concept_s b
 		on a.targetcomponentid = b.id
 	left join curr_description_s c
-        	on a.targetcomponentid = c.id
+        on a.targetcomponentid = c.id
 	left join curr_relationship_s d
 		on a.targetcomponentid = d.id
-	where b.id is null and c.id is null and d.id is null;
+	where b.id is null and c.id is null and d.id is null
+		and a.refsetid not in 
+			(900000000000523009, /* POSSIBLY EQUIVALENT TO */
+			900000000000528000, /* WAS A */
+			900000000000530003, /* ALTERNATIVE */
+			900000000000531004) /* REFERS TO */;
 	commit;
