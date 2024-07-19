05d0a72e87f9b768be6b59a50e797e9f527ed213
Quyen Ly <quocquyenit@gmail.com>
2024-07-19 20:43:40 +0700

    VAL-394 RVF [update]: association-valid-targetcomponentid should check target components, not concepts

--- scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid.sql
@@ -6,17 +6,27 @@
 	TargetComponentId refers to valid concepts in the ASSOCIATION REFSET snapshot file.
 
 ********************************************************************************/
+
+	create table if not exists v_concept_and_description_ids as
+	select id from curr_concept_s  
+	union all
+	select id from curr_description_s;
+
+	alter table v_concept_and_description_ids add index idx_concept_and_description_id(id);
+
 	insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		a.referencedcomponentid,
-		concat('ASSOC RS: Targetcomponentid=',a.targetcomponentid, ':Invalid TargetComponentId.'),
+		concat('ASSOC RS: Targetcomponentid=',a.targetcomponentid, ':Invalid TargetComponentId. RefsetId=', a.refsetid, '.'),
 		a.id,
 		'curr_associationrefset_s'
 	from curr_associationrefset_s a
-	left join curr_concept_s b
+	left join v_concept_and_description_ids b
 	on a.targetcomponentid = b.id
 	where b.id is null;
 	commit;
+
+	drop table if exists v_concept_and_description_ids;
 	
