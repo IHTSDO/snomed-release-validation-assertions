4fa9037a0eda2b9fe51bcf346b0bb694ef5ea4c1
Quyen Ly <quocquyenit@gmail.com>
2024-08-16 21:17:21 +0700

    VAL-401 Allow relationship id as target in historical association reference sets

--- scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid_AU_EDITION.sql
+++ scripts/file-centric/file-centric-snapshot-association-valid-targetcomponentid_AU_EDITION.sql
@@ -1,18 +1,20 @@
 
 /******************************************************************************** 
-	file-centric-snapshot-association-valid-targetcomponentid
+	file-centric-snapshot-association-valid-targetcomponentid_AU_EDITION
 
 	Assertion:
 	TargetComponentId refers to valid components in the ASSOCIATION REFSET snapshot file.
 
 ********************************************************************************/
 
-	create table if not exists v_concept_and_description_ids as
+	create table if not exists v_concept_and_description_and_relationship_ids as
 	select id from curr_concept_s  
 	union all
-	select id from curr_description_s;
+	select id from curr_description_s
+	union all
+	select id from curr_relationship_s;
 
-	alter table v_concept_and_description_ids add index idx_concept_and_description_id(id);
+	alter table v_concept_and_description_and_relationship_ids add index idx_concept_and_description_and_relationship_id(id);
 
 	insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
 	select 
@@ -23,10 +25,10 @@
 		a.id,
 		'curr_associationrefset_s'
 	from curr_associationrefset_s a
-	left join v_concept_and_description_ids b
+	left join v_concept_and_description_and_relationship_ids b
 	on a.targetcomponentid = b.id
 	where b.id is null;
 	commit;
 
-	drop table if exists v_concept_and_description_ids;
+	drop table if exists v_concept_and_description_and_relationship_ids;
 	
