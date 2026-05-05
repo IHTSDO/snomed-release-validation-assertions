c073a87417cedb3d53f48477d4acf4c66ff78b9b
Quyen Ly <quocquyenit@gmail.com>
2026-05-05 12:28:14 +0700

    VAL-526 Reenable checks for references to non-existent concepts in simple maps as a new assertion

---
+++ scripts/component-centric/component-centric-snapshot-referencedComponentID-of-simple-map-refset-member-must-be-valid.sql
@@ -0,0 +1,22 @@
+
+/******************************************************************************** 
+component-centric-snapshot-referencedComponentID-of-simple-map-refset-member-must-be-valid.sql
+
+	Assertion:
+	"All active simple map Refset records must be valid."
+
+********************************************************************************/
+
+
+
+    insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
+    select
+        <RUNID>,
+        '<ASSERTIONUUID>',
+        a.referencedcomponentid,
+        concat('Reference component id:',a.referencedcomponentid,' for refset id: ', a.refsetid, ' in simple map refset must be valid.'),
+        a.id,
+        'curr_simplemaprefset_s'
+    from curr_simplemaprefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
+    where a.active = '1' and (b.active = '0' or b.id is null);
+
